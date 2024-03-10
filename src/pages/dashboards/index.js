7a703ed7d1e55e16384a2346c1bb6dbd1e6036db
haywhyvilla <olatunjiayobami575@gmail.com>
2024-03-10 23:36:19 +0100

    new commit

--- src/pages/dashboards/index.js
+++ src/pages/dashboards/index.js
@@ -1,33 +1,44 @@
+import { Fragment, useEffect, useState } from 'react'
+
 // ** MUI Import
 import Grid from '@mui/material/Grid'
 
 // ** Demo Component Imports
-import LoanList from 'src/pages/dashboards/components/LoanList'
-import GuarantorsList from 'src/pages/dashboards/components/GuarantorsList'
-import Statistics from 'src/pages/dashboards/components/Statistics'
-import Welcome from 'src/pages/dashboards/components/Welcome'
+
 
 // ** Custom Component Import
 import ApexChartWrapper from 'src/@core/styles/libs/react-apexcharts'
+import FAQS from 'src/views/pages/faq/Faqs'
+import FaqHeader from 'src/views/pages/faq/FaqHeader'
+import FaqFooter from 'src/views/pages/faq/FaqFooter'
+
 
 const Dashboard = () => {
+
+
   return (
-    <ApexChartWrapper>
-      <Grid container spacing={6}>
-        <Grid item xs={12} md={4}>
-          <Welcome />
-        </Grid>
-        {/* <Grid item xs={12} md={8}>
-          <Statistics />
-        </Grid> */}
-        {/* <Grid item xs={12} lg={6}>
-          <GuarantorsList />
-        </Grid> */}
-        {/* <Grid item xs={12} lg={6}>
-          <LoanList />
-        </Grid> */}
-      </Grid>
-    </ApexChartWrapper>
+
+    // <ApexChartWrapper>
+    //   <Grid container spacing={6}>
+    //     <Grid item xs={12} md={4}>
+    //       <Welcome />
+    //     </Grid>
+    //     {/* <Grid item xs={12} md={8}>
+    //       <Statistics />
+    //     </Grid> */}
+    //     {/* <Grid item xs={12} lg={6}>
+    //       <GuarantorsList />
+    //     </Grid> */}
+    //     {/* <Grid item xs={12} lg={6}>
+    //       <LoanList />
+    //     </Grid> */}
+    //   </Grid>
+    // </ApexChartWrapper>
+    <Fragment>
+    <FaqHeader/>
+    {/* {data !== null ? <FAQS data={data} activeTab={activeTab} handleChange={handleChange} /> : renderNoResult} */}
+    <FaqFooter />
+  </Fragment>
   )
 }
 
